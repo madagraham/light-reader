8aba0e84903762250a993a0456f861a89a88d884
Adam Graham <axiom@Adams-MBP.lan>
2023-04-09 22:32:36 -0400

    added multiple recipient functionality

--- read_light_notify.py
+++ read_light_notify.py
@@ -30,13 +30,18 @@ account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 twilio_nyc = os.environ['TWILIO_NUMBER']
 recipient = os.environ['RECIPIENT_NUMBER']
+recipient_two = os.environ['RECIPIENT_TWO_NUMBER']
 
 client = Client(account_sid, auth_token)
 
-message = client.messages \
-                .create(
-                     body=today_empire_state_bldg_color,
-                     from_=twilio_nyc,
-                     to=recipient
-                 )
+users = [recipient, recipient_two]
+
+for user in users:
+    message = client.messages \
+                    .create(
+                         body=today_empire_state_bldg_color,
+                         from_=twilio_nyc,
+                         to=user
+                     )
+    print(message.sid)
 
